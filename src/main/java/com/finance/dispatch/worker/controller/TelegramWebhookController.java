45592d85292f54be7fc9e093eac30d6dd9f3e702
tanghai.ly <tanghai.ly@wingbank.com.kh>
2026-09-15 15:04:33 +0700

    update TTL and cache

--- src/main/java/com/finance/dispatch/worker/controller/TelegramWebhookController.java
+++ src/main/java/com/finance/dispatch/worker/controller/TelegramWebhookController.java
@@ -53,4 +53,56 @@ public class TelegramWebhookController {
             System.out.println("Unknown message: " + text);
         }
     }
+
+    private String processCommand(String chatId, String command) throws Exception {
+
+        log.info("incoming command {}", command);
+
+        if (command.trim().startsWith("*monthly:")) {
+            return "";
+        }
+
+        if(command.startsWith("*asset:")) {
+            if (!"678134373".equals(chatId)) {
+                return "𝙔𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤 𝙥𝙧𝙞𝙫𝙞𝙡𝙚𝙜𝙚 𝙩𝙤 𝙪𝙨𝙚 𝙩𝙝𝙞𝙨 𝙘𝙤𝙢𝙢𝙖𝙣𝙙❗";
+            } else {
+                return "";
+            }
+        }
+
+        switch (command) {
+            case "/llist":
+                return "";
+
+            case "/assets":
+                if (!"678134373".equals(chatId)) {
+                    return "𝙔𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤 𝙥𝙧𝙞𝙫𝙞𝙡𝙚𝙜𝙚 𝙩𝙤 𝙪𝙨𝙚 𝙩𝙝𝙞𝙨 𝙘𝙤𝙢𝙢𝙖𝙣𝙙❗";
+                } else {
+                    return "";
+                }
+                //-----------------------------------------------------------------
+
+            case "/help":
+                return
+                        "*🤖 Ｂｏｔ Ｃｏｍｍａｎｄｓ Ｈｅｌｐ*\n\n" +
+                                "📅 /calendar \\- Show this week's important events (US)\n" +
+                                "💰 /gold \\- Show the real\\-time live price of gold\n" +
+                                "🔔 /subscribe \\- Receive alerts and important announcements\n" +
+                                "❌ /unsubscribe \\- Stop receiving alerts and announcements\n" +
+
+                                "📊 /budget \\- Check monthly budget breakdown\n" +
+                                "⭐ /clsbud \\- Clear monthly budget\n" +
+
+                                "🔁 /llist \\- List recurring alerts\n" +
+                                "⏰ /loop \\- Manage looping reminders\n" +
+                                "• *Add:* `/loop +10m drink water`\n" +
+                                "• *Remove:* `/loop - 1`\n" +
+                                "• *Clear:* `/loop *`\n" +
+                                "⭐ /assettemplate \\-Get Asset Register Template\n" +
+                                "💡 *Tip:* _Use the commands exactly as shown above._\n\n";
+
+            default:
+                return "";
+        }
+    }
 }
