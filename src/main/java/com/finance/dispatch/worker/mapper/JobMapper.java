b6dcee6f24f39b52664ede4cd7840073eb3463ad
tanghai.ly <tanghai.ly@wingbank.com.kh>
2026-09-15 11:44:17 +0700

    remove response mapper

--- src/main/java/com/finance/dispatch/worker/mapper/JobMapper.java
+++ src/main/java/com/finance/dispatch/worker/mapper/JobMapper.java
@@ -6,12 +6,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
-import java.time.LocalDateTime;
-
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface JobMapper {
-
-    JobResponse toMapCreateNewJobResponse(ScheduledJob scheduledJob, LocalDateTime createdAt);
 
     JobResponse toResponse(ScheduledJob scheduledJob);
 }
